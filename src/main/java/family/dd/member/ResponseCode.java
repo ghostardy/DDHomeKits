2f9004dc7e532b5b4dc1cf9ad9fcccd66bcf71c0
ghostardy <gtsb-886@163.com>
2020-11-19 11:14:03 +0800

    Create ResponseCode.java

---
+++ src/main/java/family/dd/member/ResponseCode.java
@@ -0,0 +1,16 @@
+package family.dd.member;
+
+public enum ResponseCode {
+    HANDLING(100),
+    SUCCESS(200),
+    REQUEST_FAILURE(400),
+    REQUEST_UNAUTHORIZED(401),
+    REQUEST_FORBIDDEN(403),
+    SERVER_ERROR(500),
+    UNKNOWN(999);
+
+    private int code;
+    ResponseCode(int code){
+        this.code = code;
+    }
+}
