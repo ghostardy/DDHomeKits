767ae14f4d22560fc420cc8577853a8bcd707d2a
ghostardy <gtsb-886@163.com>
2020-11-19 17:07:18 +0800

    Update FamilyMemberDAO.java

--- src/main/java/family/dd/member/FamilyMemberDAO.java
+++ src/main/java/family/dd/member/FamilyMemberDAO.java
@@ -1,4 +1,9 @@
 package family.dd.member;
 
+import family.dd.member.entity.UserInfo;
+
 public class FamilyMemberDAO {
+    UserInfo[] getUserInfo(String account, String encryptedPwd) {
+        return new UserInfo[]{new UserInfo()};
+    }
 }
