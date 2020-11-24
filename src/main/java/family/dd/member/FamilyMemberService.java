1701163b06a55cdb8303246cf4e6f24fe4b31cf6
ghostardy <gtsb-886@163.com>
2020-11-24 18:58:11 +0800

    Update FamilyMemberService.java

--- src/main/java/family/dd/member/FamilyMemberService.java
+++ src/main/java/family/dd/member/FamilyMemberService.java
@@ -1,4 +1,13 @@
 package family.dd.member;
 
 public class FamilyMemberService {
+    FamilyMember familyMember;
+
+    public String login(){
+        String username = "username";
+        String password = "password";
+
+        familyMember.login("username", "password");
+        return new ServiceResponse().toJson();
+    }
 }
