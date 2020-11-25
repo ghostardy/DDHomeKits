5fe9529a2dcb0c84612e169fad2b85d31584159c
ghostardy <gtsb-886@163.com>
2020-11-25 16:27:57 +0800

    Update FamilyMember.java

--- src/main/java/family/dd/member/FamilyMember.java
+++ src/main/java/family/dd/member/FamilyMember.java
@@ -7,6 +7,8 @@ import family.dd.entity.UserInfo;
 import family.dd.entity.UserRole;
 import family.dd.member.handler.LoginHandler;
 import family.dd.member.handler.LoginHandlerFactory;
+import family.dd.member.handler.PasswordHandler;
+import family.dd.member.handler.PasswordHandlerFactory;
 import family.dd.member.role.FamilyRole;
 
 import java.util.HashSet;
@@ -20,6 +22,7 @@ import static family.dd.common.util.CommonUtil.isNull;
 public class FamilyMember {
     private FamilyRole role;
     private FamilyMemberRepository repository ;
+    private LoginHandlerFactory loginHandlerFactory;
 
 
     HandleResult recover(String memberId){
@@ -33,13 +36,23 @@ public class FamilyMember {
      * @return HandleResult
      */
     public HandleResult login(String username, String password){
-        String encryptedPassword = "password handled by PasswordHandler" + password;
-        UserInfo[] users = repository.getUserInfo(username, encryptedPassword);
+        UserInfo[] users = repository.getUserInfo(username);
+
         if (users.length!=1){
             return new HandleResult(ResponseCode.REQUEST_UNAUTHORIZED, "Invalid account or password");
         }else {
-            LoginHandler handlerChain = new LoginHandlerFactory().getInstance();
-            return handlerChain.handle(users[0]);
+            int pwdStrategy = users[0].getExtraInfo().getPwdStrategy();
+            PasswordHandler pwdHandler = new PasswordHandlerFactory().getInstance(pwdStrategy);
+            HandleResult pwdHandleResult = pwdHandler.handle(new UserInfo(username, password));
+
+            if (!pwdHandleResult.getCode().equals(ResponseCode.SUCCESS)){
+                return new HandleResult(ResponseCode.SERVER_ERROR, "Login Failed! Detail : " + pwdHandleResult.getMessage());
+            } else if (!pwdHandleResult.getMessage().equals(users[0].getPassword())) {
+                return new HandleResult(ResponseCode.REQUEST_UNAUTHORIZED, "Invalid account or password");
+            } else {
+                LoginHandler loginHandler = loginHandlerFactory.getInstance();
+                return loginHandler.handle(users[0]);
+            }
         }
     }
 
@@ -63,7 +76,7 @@ public class FamilyMember {
     }
 
     UserInfo getUserInfo(int memberId) {
-        return new UserInfo();
+        return new UserInfo("","");
     }
 
     boolean checkPassword(UserInfo user, String password){
