927b9e06bc1d03e14d1d2e7303e0b610cb5852a7
ghostardy <gtsb-886@163.com>
2020-11-25 16:28:32 +0800

    Update FamilyMemberRepository.java

--- src/main/java/family/dd/member/FamilyMemberRepository.java
+++ src/main/java/family/dd/member/FamilyMemberRepository.java
@@ -4,10 +4,10 @@ import family.dd.entity.UserInfo;
 
 class FamilyMemberRepository {
     private FamilyMemberDAO dao;
-    UserInfo[] getUserInfo(String account, String encryptedPwd) {
-        return new UserInfo[]{new UserInfo()};
+    UserInfo[] getUserInfo(String account) {
+        return new UserInfo[]{new UserInfo("","")};
     }
     UserInfo[] getUserInfo(int memberId) {
-        return new UserInfo[]{new UserInfo()};
+        return new UserInfo[]{new UserInfo("","")};
     }
 }
