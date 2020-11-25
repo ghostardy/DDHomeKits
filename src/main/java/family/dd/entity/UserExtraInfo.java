5d8bd373a3d1443280c534e55da8b84abc6f1aad
ghostardy <gtsb-886@163.com>
2020-11-25 16:25:22 +0800

    Create UserExtraInfo.java

---
+++ src/main/java/family/dd/entity/UserExtraInfo.java
@@ -0,0 +1,17 @@
+package family.dd.entity;
+
+public class UserExtraInfo {
+    /**
+     * Version of password safe strategy
+     * Current default version is 1
+     */
+    private int pwdStrategy=1;
+
+    public int getPwdStrategy() {
+        return pwdStrategy;
+    }
+
+    public void setPwdStrategy(int pwdStrategy) {
+        this.pwdStrategy = pwdStrategy;
+    }
+}
