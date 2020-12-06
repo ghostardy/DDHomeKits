320c87c7f88c66528a437b8ce52f8ff8616222d1
Ghostardy <gtsb-886@163.com>
2020-12-06 12:32:08 +0800

    Move project to springboot

--- src/main/java/family/dd/DDHomeKits/member/FamilyMemberService.java
+++ src/main/java/family/dd/DDHomeKits/member/FamilyMemberService.java
@@ -1,13 +1,23 @@
 package family.dd.DDHomeKits.member;
 
-public class FamilyMemberService {
-    FamilyMember familyMember;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
+public class FamilyMemberService {
+    FamilyMember familyMember = new FamilyMember();
+
+    @GetMapping("/login")
     public String login(){
         String username = "username";
         String password = "password";
 
-        familyMember.login("username", "password");
-        return new ServiceResponse().toJson();
+        //familyMember.login("username", "password");
+        //return new ServiceResponse().toJson();
+        return "Hello";
     }
 }
