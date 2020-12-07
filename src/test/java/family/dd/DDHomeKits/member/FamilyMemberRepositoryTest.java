2c952448d72f2d5ed5cd3b1eadcb8a34a19179cd
ghostardy <gtsb-886@163.com>
2020-12-07 15:48:56 +0800

    Update FamilyMemberRepositoryTest.java

--- src/test/java/family/dd/DDHomeKits/member/FamilyMemberRepositoryTest.java
+++ src/test/java/family/dd/DDHomeKits/member/FamilyMemberRepositoryTest.java
@@ -1,14 +1,24 @@
 package family.dd.DDHomeKits.member;
 
+import family.dd.DDHomeKits.DDHomeKitsApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DDHomeKitsApplication.class)
 class FamilyMemberRepositoryTest {
+    @Autowired
+    FamilyMemberRepository repository;
 
     @Test
     void getUserInfo() {
-    }
-
-    @Test
-    void getUserInfo1() {
+        assertEquals(new ArrayList(), repository.getUserInfo(1));
     }
 }
