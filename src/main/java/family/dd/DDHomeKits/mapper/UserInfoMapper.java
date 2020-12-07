f5bb3b1f45b752fea964d325d6208a1fe8c188e4
ghostardy <gtsb-886@163.com>
2020-12-07 15:53:33 +0800

    Create UserInfoMapper.java

---
+++ src/main/java/family/dd/DDHomeKits/mapper/UserInfoMapper.java
@@ -0,0 +1,50 @@
+package family.dd.DDHomeKits.mapper;
+
+import family.dd.DDHomeKits.entity.UserInfo;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface UserInfoMapper {
+    @Select("Select * from UserInfo where userId=#{userId}")
+    @Results({
+            @Result(property="userId", column="userId"),
+            @Result(property="username", column="username"),
+            @Result(property="password", column="password"),
+            @Result(property="nickName", column="nickName"),
+            @Result(property="status", column="status", typeHandler = family.dd.DDHomeKits.mapper.UserStatusTypeHandler.class),
+            @Result(property="authority", column="authority"),
+            @Result(property="extraInfo", column="extraInfo"),
+            @Result(property="signUpDate", column="signUpDate")
+    })
+    List<UserInfo> findUserById(int userId);
+
+    @Select("Select * from UserInfo where account=#{account}")
+    @Results({
+            @Result(property="userId", column="userId"),
+            @Result(property="username", column="username"),
+            @Result(property="password", column="password"),
+            @Result(property="nickName", column="nickName"),
+            @Result(property="status", column="status", typeHandler = family.dd.DDHomeKits.mapper.UserStatusTypeHandler.class),
+            @Result(property="authority", column="authority"),
+            @Result(property="extraInfo", column="extraInfo"),
+            @Result(property="signUpDate", column="signUpDate")
+    })
+    List<UserInfo> findUserByAccount(String account);
+
+    @Insert("Insert into UserInfo(username," +
+            "password," +
+            "nickName," +
+            "status," +
+            "authority," +
+            "extraInfo," +
+            "signUpDate) values(#{userInfo.username}," +
+            "#{userInfo.password}，" +
+            "#{userInfo.nickName}，" +
+            "#{userInfo.status,typeHandler=family.dd.DDHomeKits.mapper.UserStatusTypeHandler}，" +
+            "#{userInfo.authority}，" +
+            "#{userInfo.extraInfo}，" +
+            "#{userInfo.signUpDate}")
+    void addUser(UserInfo userInfo);
+}
