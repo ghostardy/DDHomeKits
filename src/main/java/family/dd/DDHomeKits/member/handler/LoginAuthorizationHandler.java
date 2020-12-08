894e5f5fc17bcaba4179d785a120b4314a66eba2
ghostardy <gtsb-886@163.com>
2020-12-08 18:54:57 +0800

    Update LoginAuthorizationHandler.java

--- src/main/java/family/dd/DDHomeKits/member/handler/LoginAuthorizationHandler.java
+++ src/main/java/family/dd/DDHomeKits/member/handler/LoginAuthorizationHandler.java
@@ -8,7 +8,7 @@ import family.dd.DDHomeKits.entity.UserInfo;
 class LoginAuthorizationHandler extends LoginHandler {
     public HandleResult handle(UserInfo user){
         if(Privilege.hasPermission(0L, Privilege.LOG_IN)) {
-            return new HandleResult(ResponseCode.REQUEST_UNAUTHORIZED, "Family member login succeed");
+            return new HandleResult(ResponseCode.REQUEST_UNAUTHORIZED, "Family member is unauthorized");
         } else {
             return next.handle(user);
         }
