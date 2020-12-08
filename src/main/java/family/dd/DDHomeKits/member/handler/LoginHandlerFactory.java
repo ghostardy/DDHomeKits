88d8a711a49b796cf5d379cfe32ede4ceeec2d53
ghostardy <gtsb-886@163.com>
2020-12-08 18:56:24 +0800

    Update LoginHandlerFactory.java

--- src/main/java/family/dd/DDHomeKits/member/handler/LoginHandlerFactory.java
+++ src/main/java/family/dd/DDHomeKits/member/handler/LoginHandlerFactory.java
@@ -1,11 +1,19 @@
 package family.dd.DDHomeKits.member.handler;
 
 public class LoginHandlerFactory {
-    public LoginHandler getInstance() {
-        LoginHandler step1 = new LoginUserStatusHandler();
+    public static LoginHandler getInstance() {
+        /**
+         * LoginRequestHandler must be the first handler in order to get full user information
+         */
+        LoginHandler step1 = new LoginRequestHandler();
         LoginHandler step2 = new LoginAuthorizationHandler();
-        LoginHandler finalStep = new LoginSuccessHandler();
-        step1.setNext(step2).setNext(finalStep);
+        LoginHandler step3 = new LoginUserStatusHandler();
+        LoginHandler step4 = new LoginSuccessHandler();
+        /**
+         * Create handle chain
+         */
+        step1.setNext(step2).setNext(step3).setNext(step4);
+
         return step1;
     }
 }
