baeed9a5d5f04fd24be7d359826362743f9ceea1
ghostardy <gtsb-886@163.com>
2020-12-10 10:24:59 +0800

    Update LoginHandler.java

--- src/main/java/family/dd/DDHomeKits/member/handler/LoginHandler.java
+++ src/main/java/family/dd/DDHomeKits/member/handler/LoginHandler.java
@@ -6,9 +6,7 @@ import family.dd.DDHomeKits.member.FamilyMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
 public abstract class LoginHandler {
-    protected FamilyMemberRepository familyMemberRepository;
     protected LoginHandler next;
     protected LoginHandler setNext(LoginHandler next){
         return this.next = next;
@@ -17,8 +15,4 @@ public abstract class LoginHandler {
         return next;
     }
     public abstract HandleResult handle(UserInfo user);
-    @Autowired
-    public void setRepository(FamilyMemberRepository familyMemberRepository){
-        this.familyMemberRepository = familyMemberRepository;
-    }
 }
