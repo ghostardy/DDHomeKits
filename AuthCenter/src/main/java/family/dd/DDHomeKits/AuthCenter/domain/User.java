1c67598eb05895197b92ed464eef727b561156fd
ghostardy <gtsb-886@163.com>
2021-01-14 16:52:15 +0800

    Update User.java

--- AuthCenter/src/main/java/family/dd/DDHomeKits/AuthCenter/domain/User.java
+++ AuthCenter/src/main/java/family/dd/DDHomeKits/AuthCenter/domain/User.java
@@ -5,12 +5,15 @@ import family.dd.DDHomeKits.AuthCenter.dao.UserRepository;
 import family.dd.DDHomeKits.AuthCenter.dao.UserIdentityPO;
 import family.dd.DDHomeKits.AuthCenter.definition.ResponseCode;
 import family.dd.DDHomeKits.AuthCenter.dao.HandleResult;
+import family.dd.DDHomeKits.AuthCenter.definition.UserStatus;
 import family.dd.DDHomeKits.AuthCenter.domain.handler.PasswordHandler;
 import family.dd.DDHomeKits.AuthCenter.domain.handler.PasswordHandlerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerErrorException;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 /**
@@ -19,83 +22,81 @@ import java.util.List;
 @Component
 @Scope("prototype")
 public class User {
-    private UserRepository repository;
-    private UserIdentityPO userIdentity;
+    //private UserRepository repository;
+    private int userId;
+    private String username;
+    private UserStatus status;
+    private long authority;
+    private Timestamp signUpDate;
+    private String sessionId;
 
-    public UserIdentityPO getUserIdentity() {
-        return userIdentity;
+    public String getSessionId() {
+        return sessionId;
     }
 
-    public void setUserIdentity(UserIdentityPO userIdentity) {
-        this.userIdentity = userIdentity;
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
     }
 
     private Gson gson = new Gson();
+
+    public User(){}
+    public User(UserIdentityPO userPO){
+        this.userId = userPO.getUserId();
+        this.username = userPO.getUsername();
+        this.status = userPO.getStatus();
+        this.authority = userPO.getAuthority();
+        this.signUpDate = userPO.getSignUpDate();
+    }
 
     HandleResult recover(String memberId){
         return new HandleResult(ResponseCode.SUCCESS, "Family member login status is recovered");
     }
 
-    /**
-     * Authorize user and put user info into cache
-     * @param username username inputted by front user
-     * @param password password inputted by front user
-     * @return HandleResult
-     */
-    public HandleResult authenticate(String username, String password){
-        /**
-         * Check password inputted by front user
-         */
-        String handledPassword;
-        try{
-            handledPassword = transPassword(username, password);
-        }catch (Exception e) {
-            return new HandleResult(ResponseCode.SERVER_ERROR, "Server ERROR, Login Failed!");
-        }
-        List<UserIdentityPO> members = repository.find(username, handledPassword);
 
-        if(members.size() != 1) {
-            return new HandleResult(ResponseCode.REQUEST_FAILURE, "Authenticate Failed! Invalid username or password");
-        }
 
-        userIdentity = members.get(0);
-        return new HandleResult(ResponseCode.SUCCESS, gson.toJson(userIdentity));
-    }
-    /**
-     * Handle password inputted by front user
-     * @param username Use username as seed of password handler
-     * @param password unhandled password
-     * @throws Exception when failed to handle password
-     * @return handled password
-     */
-    public String transPassword(String username, String password) throws Exception{
-        PasswordHandler pwdHandler = PasswordHandlerFactory.getInstance();
-        return pwdHandler.handle(password, username);
-    }
-    /**
-     * Sign up new user, only used by inner service
-     * @param userInfo
-     * @return HandleResult
-     */
-    public HandleResult singUp(UserIdentityPO userInfo){
-        UserIdentityPO newUser = new UserIdentityPO(userInfo);
-        List<UserIdentityPO> users = repository.find(newUser.getUsername());
-        if(users.size() != 0) {
-            return new HandleResult(ResponseCode.REQUEST_FAILURE, "Can not sign up by username: " +
-                    newUser.getUsername() + ". Please try another one.");
-        }
+    //@Autowired
+    //public void setRepository(UserRepository repository) {
+        //this.repository = repository;
+    //}
 
-        try{
-            newUser.setPassword(transPassword(newUser.getUsername(), newUser.getPassword()));
-            repository.add(newUser);
-        } catch (Exception e){
-            return new HandleResult(ResponseCode.SERVER_ERROR, "Failed to sign up user, please try again later");
-        }
-        return new HandleResult(ResponseCode.SUCCESS, "Thanks for Signing Up");
+    public int getUserId() {
+        return userId;
     }
 
-    @Autowired
-    public void setRepository(UserRepository repository) {
-        this.repository = repository;
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(UserStatus status) {
+        this.status = status;
+    }
+
+    public long getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(long authority) {
+        this.authority = authority;
+    }
+
+    public Timestamp getSignUpDate() {
+        return signUpDate;
+    }
+
+    public void setSignUpDate(Timestamp signUpDate) {
+        this.signUpDate = signUpDate;
     }
 }
