1aa61f08a6258931e58be9aededd6986d4db9d3d
ghostardy <gtsb-886@163.com>
2021-01-14 16:49:09 +0800

    Update FamilyMemberController.java

--- AuthCenter/src/main/java/family/dd/DDHomeKits/AuthCenter/controller/FamilyMemberController.java
+++ AuthCenter/src/main/java/family/dd/DDHomeKits/AuthCenter/controller/FamilyMemberController.java
@@ -1,9 +1,12 @@
 package family.dd.DDHomeKits.AuthCenter.controller;
 
 import family.dd.DDHomeKits.AuthCenter.dao.UserIdentityPO;
-import family.dd.DDHomeKits.AuthCenter.domain.User;
+import family.dd.DDHomeKits.AuthCenter.definition.ResponseCode;
+import family.dd.DDHomeKits.AuthCenter.domain.Authenticator;
+import family.dd.DDHomeKits.AuthCenter.domain.ServiceResponseDTO;
+import family.dd.DDHomeKits.AuthCenter.domain.UserManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @Scope("prototype")
 public class FamilyMemberController {
 
-    private User user;
+    private Authenticator authenticator;
+    private UserManager userManager;
     @Autowired
-    public void setUser(User user) {
-        this.user = user;
+    public void setAuthenticator(Authenticator authenticator) {
+        this.authenticator = authenticator;
+    }
+    @Autowired
+    public void setUserManager(UserManager userManager) {
+        this.userManager = userManager;
     }
 
     @GetMapping("/login")
     public String login(){
-        return user.authenticate("username", "password").getMessage();
+        try {
+            return authenticator.authenticate("username", "password").toString();
+        }catch (Exception e) {
+            return new ServiceResponseDTO(ResponseCode.REQUEST_FAILURE, "Authenticate Failed! Invalid username or password", null).toJson();
+        }
+
     }
     @GetMapping("/signUp")
     public String signUp(){
@@ -28,6 +41,6 @@ public class FamilyMemberController {
         userIdentityPO.setUsername("username");
         userIdentityPO.setPassword("password");
 
-        return user.singUp(userIdentityPO).getMessage();
+        return userManager.singUp(userIdentityPO).getMessage();
     }
 }
