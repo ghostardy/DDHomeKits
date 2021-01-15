52cad9f5276da06a11a32b7ca09df1cc34d06760
ghostardy <gtsb-886@163.com>
2021-01-15 16:24:03 +0800

    Update CommonUtil.java

--- AuthCenter/src/main/java/family/dd/DDHomeKits/AuthCenter/util/CommonUtil.java
+++ AuthCenter/src/main/java/family/dd/DDHomeKits/AuthCenter/util/CommonUtil.java
@@ -12,20 +12,4 @@ public class CommonUtil {
     public static boolean isEmptyString(String str) {
         return null==str||str.isEmpty()||str.equals("");
     }
-
-    /**
-     * Handle password inputted by front user
-     * @param username Use username as seed of password handler
-     * @param password unhandled password
-     * @throws Exception when failed to handle password
-     * @return handled password
-     */
-    public static String transPassword(String username, String password) throws HandlePasswordException {
-        try{
-            PasswordHandler pwdHandler = PasswordHandlerFactory.getInstance();
-            return pwdHandler.handle(password, username);
-        }catch (Exception e) {
-            throw new HandlePasswordException("Server ERROR, Login Failed!");
-        }
-    }
 }
