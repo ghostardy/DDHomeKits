d0563b3bac0b9cf72c7a47d59123892ef4f7551a
ghostardy <gtsb-886@163.com>
2021-01-15 16:23:27 +0800

    Update UserManager.java

--- AuthCenter/src/main/java/family/dd/DDHomeKits/AuthCenter/domain/UserManager.java
+++ AuthCenter/src/main/java/family/dd/DDHomeKits/AuthCenter/domain/UserManager.java
@@ -4,7 +4,8 @@ import family.dd.DDHomeKits.AuthCenter.dao.HandleResult;
 import family.dd.DDHomeKits.AuthCenter.dao.UserIdentityPO;
 import family.dd.DDHomeKits.AuthCenter.dao.UserRepository;
 import family.dd.DDHomeKits.AuthCenter.definition.ResponseCode;
-import family.dd.DDHomeKits.AuthCenter.util.CommonUtil;
+import family.dd.DDHomeKits.AuthCenter.domain.handler.PasswordHandler;
+import family.dd.DDHomeKits.AuthCenter.domain.handler.PasswordHandlerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,24 +20,27 @@ public class UserManager {
         this.repository = repository;
     }
     /**
-     * Sign up new user, only used by inner service
-     * @param userInfo
+     * Add new user, only used by inner service
+     * @param newUserInfo
+     * @param encryptedPassword
      * @return HandleResult
      */
-    public HandleResult singUp(UserIdentityPO userInfo){
-        UserIdentityPO newUser = new UserIdentityPO(userInfo);
-        List<UserIdentityPO> users = repository.find(newUser.getUsername());
-        if(users.size() != 0) {
-            return new HandleResult(ResponseCode.REQUEST_FAILURE, "Can not sign up by username: " +
-                    newUser.getUsername() + ". Please try another one.");
-        }
+    public User addUser(User newUserInfo, String encryptedPassword){
+        UserIdentityPO newUserPO = new UserIdentityPO();
+        newUserPO.setUsername(newUserInfo.getUsername());
+        newUserPO.setPassword(encryptedPassword);
+        newUserPO.setStatus(newUserInfo.getStatus());
+        newUserPO.setAuthority(newUserInfo.getAuthority());
+        newUserPO.setSignUpDate(newUserInfo.getSignUpDate());
+        repository.add(newUserPO);
+        return usernameExists(newUserPO.getUsername());
+    }
 
-        try{
-            newUser.setPassword(CommonUtil.transPassword(newUser.getUsername(), newUser.getPassword()));
-            repository.add(newUser);
-        } catch (Exception e){
-            return new HandleResult(ResponseCode.SERVER_ERROR, "Failed to sign up user, please try again later");
+    public User usernameExists(String username) {
+        List<UserIdentityPO> users = repository.find(username);
+        if(users.size() != 0) {
+            return new User(users.get(0));
         }
-        return new HandleResult(ResponseCode.SUCCESS, "Thanks for Signing Up");
+        return null;
     }
 }
