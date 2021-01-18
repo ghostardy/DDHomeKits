8ac6c63c4991e9e471fa349fbadf9d8e1d579b5f
ghostardy <gtsb-886@163.com>
2021-01-18 17:23:07 +0800

    Update Authenticator.java

--- AuthCenter/src/main/java/family/dd/DDHomeKits/AuthCenter/domain/Authenticator.java
+++ AuthCenter/src/main/java/family/dd/DDHomeKits/AuthCenter/domain/Authenticator.java
@@ -1,9 +1,9 @@
 package family.dd.DDHomeKits.AuthCenter.domain;
 
+import com.google.gson.Gson;
 import family.dd.DDHomeKits.AuthCenter.dao.UserIdentityPO;
 import family.dd.DDHomeKits.AuthCenter.dao.UserRepository;
-import family.dd.DDHomeKits.AuthCenter.domain.handler.PasswordHandler;
-import family.dd.DDHomeKits.AuthCenter.domain.handler.PasswordHandlerFactory;
+import family.dd.DDHomeKits.AuthCenter.definition.Privilege;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,35 +21,22 @@ public class Authenticator {
     /**
      * Authorize user and put user info into cache
      * @param username username inputted by front user
-     * @param password password inputted by front user
+     * @param encryptedPassword encrypted password inputted by front user
      * @return HandleResult
      */
-    public User authenticate(String username, String password) throws HandlePasswordException{
+    public User authenticate(String username, String encryptedPassword){
         /**
          * Check password inputted by front user
          */
-        String handledPassword = transPassword(username, password);
-
-        List<UserIdentityPO> members = repository.find(username, handledPassword);
+        List<UserIdentityPO> members = repository.find(username, encryptedPassword);
 
         if(members.size() == 1) {
             return new User(members.get(0));
         }
         return null;
     }
-    /**
-     * Handle password inputted by front user
-     * @param username Use username as seed of password handler
-     * @param password unhandled password
-     * @throws Exception when failed to handle password
-     * @return handled password
-     */
-    public static String transPassword(String username, String password) throws HandlePasswordException {
-        try{
-            PasswordHandler pwdHandler = PasswordHandlerFactory.getInstance();
-            return pwdHandler.handle(password, username);
-        }catch (Exception e) {
-            throw new HandlePasswordException("Server ERROR, Login Failed!");
-        }
+
+    public boolean hasPermission(User user, Privilege p) {
+        return Privilege.hasPermission(user.getAuthority(), p);
     }
 }
