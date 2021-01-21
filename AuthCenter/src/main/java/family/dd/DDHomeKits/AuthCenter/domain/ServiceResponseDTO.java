003308d1d5e21ea55d04a2965b24bb7d56cb10a6
ghostardy <gtsb-886@163.com>
2021-01-21 09:36:37 +0800

    Update ServiceResponseDTO.java

--- AuthCenter/src/main/java/family/dd/DDHomeKits/AuthCenter/domain/ServiceResponseDTO.java
+++ AuthCenter/src/main/java/family/dd/DDHomeKits/AuthCenter/domain/ServiceResponseDTO.java
@@ -1,46 +1,62 @@
-package family.dd.DDHomeKits.AuthCenter.domain;
+package family.dd.DDHomeKits.AuthCenter.controller;
 
 import com.google.gson.Gson;
 import family.dd.DDHomeKits.AuthCenter.definition.ResponseCode;
+import family.dd.DDHomeKits.AuthCenter.domain.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class ServiceResponseDTO {
-    private ResponseCode code= ResponseCode.HANDLING;
-    private String message="handling";
-    private String data="{}";
-    private Gson gson;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-    public ServiceResponseDTO() {}
-    public ServiceResponseDTO(ResponseCode responseCode, String message, Object data){
-        this.code = responseCode;
-        this.message = message;
-        this.data = gson.toJson(data);
+import static family.dd.DDHomeKits.AuthCenter.util.CommonUtil.isNull;
+
+@RestController
+@Scope("prototype")
+public class FamilyMemberController {
+    private UserAuthentication userAuthentication;
+    private Gson gson = new Gson();
+
+    @Autowired
+    public void setUserAuthentication (UserAuthentication userAuthentication) {
+        this.userAuthentication = userAuthentication;
     }
 
-    public ResponseCode getCode() {
-        return code;
+    @PostMapping("/login")
+    public String login(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password){
+        ServiceResponseDTO resp = new ServiceResponseDTO();
+        try {
+            User user = userAuthentication.authenticate(username, password);
+            if (isNull(user)){
+                resp.setCode(ResponseCode.REQUEST_UNAUTHORIZED);
+                resp.setMessage("Authenticate Failed! Invalid username or password");
+            }else if (!userAuthentication.isPrepared()) {
+                resp.setCode(ResponseCode.REQUEST_FORBIDDEN);
+                resp.setMessage("User is not prepared now, please try again later");
+            }else if (!userAuthentication.canLogin()) {
+                resp.setCode(ResponseCode.REQUEST_UNAUTHORIZED);
+                resp.setMessage("Not allowed to log in now, please try again later");
+            }else {
+                resp.setCode(ResponseCode.SUCCESS);
+                resp.setMessage("SUCCESS");
+                resp.setData(user);
+            }
+        }catch (Exception e) {
+            resp.setCode(ResponseCode.SERVER_ERROR);
+            resp.setMessage("Server error, please try again later");
+        }
+        return resp.toJson();
     }
+    @GetMapping("/signUp")
+    public String signUp(){
+        User newUser = new User();
+        newUser.setUsername("username");
 
-    public void setCode(ResponseCode code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    public String toJson(){
-        return gson.toJson(this);
+        try {
+            return userAuthentication.signUp(newUser, "password").getUsername();
+        }catch (Exception e) {}
+        return "Wrong";
     }
 }
